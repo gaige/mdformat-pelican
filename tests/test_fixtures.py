913417229491c2109e3f611449be77525b848256
Gaige B. Paulsen <gaige@cluetrust.com>
2024-06-29 12:01:59 -0400

    test: fix tests for gfm

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -4,12 +4,26 @@ from markdown_it.utils import read_fixture_file
 import mdformat
 import pytest
 
+try:
+  import mdformat_gfm
+  have_gfm = True
+except ImportError:
+  have_gfm = False
+
 FIXTURE_PATH = Path(__file__).parent / "fixtures.md"
 fixtures = read_fixture_file(FIXTURE_PATH)
 
 
+@pytest.mark.skipif(have_gfm, reason="testing with gfm")
 @pytest.mark.parametrize("line,title,text,expected", fixtures, ids=[f[1] for f in fixtures])
 def test_fixtures(line, title, text, expected):
     output = mdformat.text(text, extensions={"pelican"})
+    print(output)
+    assert output.rstrip() == expected.rstrip(), output
+
+@pytest.mark.skipif(not have_gfm, reason="testing without gfm")
+@pytest.mark.parametrize("line,title,text,expected", fixtures, ids=[f[1] for f in fixtures])
+def test_fixtures_gfm(line, title, text, expected):
+    output = mdformat.text(text, extensions={"pelican","gfm"})
     print(output)
     assert output.rstrip() == expected.rstrip(), output
